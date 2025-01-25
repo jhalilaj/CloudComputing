2869f2112dfb3d37d1ab0b8607d4d062f76f78ef
jhalilaj <jhalilaj@york.citycollege.eu>
2025-01-25 15:09:08 +0200

    fix

---
+++ src/main/java/com/cloud/demo/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.cloud.demo.config;
+
+import java.beans.Customizer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/appointments/**").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(withDefaults()); // Use static import
+
+        return http.build();
+    }
+}

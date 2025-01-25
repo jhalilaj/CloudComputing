1a34a1e547173a6949d168fcf6e2e47e009dc76e
jhalilaj <jhalilaj@york.citycollege.eu>
2025-01-25 16:31:58 +0200

    f

--- src/main/java/com/cloud/gateway/GatewayConfig.java
+++ src/main/java/com/cloud/gateway/GatewayConfig.java
@@ -1,36 +1,18 @@
 package com.cloud.gateway;
-
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class GatewayConfig {
-
-    @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        return http
-                .csrf().disable()
-                .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/auth/**").permitAll()
-                        .anyExchange().authenticated())
-                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
-                .build();
-    }
-
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("appointments_route", r -> r
-                        .path("/api/appointments/**")
-                        .filters(f -> f
-                                .stripPrefix(2)
-                                .addRequestHeader("X-Forwarded-Host", "gateway"))
-                        // Load balancing is automatically enabled by "lb://" scheme
-                        .uri("lb://appointments-service"))
-                .build();
+            .route("appointments", r -> r
+                .path("/api/appointments/**")
+                .uri("lb://APPOINTMENT-SERVICE")
+            )
+            .build();
     }
 }

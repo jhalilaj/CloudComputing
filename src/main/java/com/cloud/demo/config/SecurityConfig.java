66ee0a122dda6d627feadc763830bb0b5aa06a45
jhalilaj <jhalilaj@york.citycollege.eu>
2025-01-25 15:17:27 +0200

    fix

--- src/main/java/com/cloud/demo/config/SecurityConfig.java
+++ src/main/java/com/cloud/demo/config/SecurityConfig.java
@@ -1,26 +1,20 @@
 package com.cloud.demo.config;
 
-import java.beans.Customizer;
+
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/appointments/**").permitAll()
-                        .anyRequest().authenticated())
-                .httpBasic(withDefaults()); // Use static import
-
-        return http.build();
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange(exchange -> exchange.anyExchange().permitAll()).build();
     }
 }
